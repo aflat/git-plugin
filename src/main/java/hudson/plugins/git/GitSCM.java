8babaab2b563c89ac7b360440bb40b6103be058e
aflat <ghstock@gmail.com>
2018-06-25 10:56:33 -0400

    more whitespace removal

--- src/main/java/hudson/plugins/git/GitSCM.java
+++ src/main/java/hudson/plugins/git/GitSCM.java
@@ -1416,7 +1416,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
                 count++;
             }
         }
-       
+
         getDescriptor().populateEnvironmentVariables(env);
         for (GitSCMExtension ext : extensions) {
             ext.populateEnvironmentVariables(this, env);
